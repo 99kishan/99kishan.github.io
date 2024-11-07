2f07246e32316bbfa450183365d919bd8dae9917
99kishan <kishan99.b@gmail.com>
2024-11-07 19:24:00 +0000

    TRIAL #4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: 'https://github.com/99kishan/99kishan.github.io', // Ensures relative paths for GitHub Pages
+  build: {
+    outDir: 'dist', // Specify output directory if necessary
+    rollupOptions: {
+      input: './src/main.jsx', // Adjust to point to your main entry file if renamed
+    }
+  }
+});
