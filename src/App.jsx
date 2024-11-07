b4a8168706e73eab0387a1b48f7d3879c38e0a45
99kishan <kishan99.b@gmail.com>
2024-11-07 22:02:26 +0000

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,10 @@ import ParticlesComponent from './components/ParticlesComponent';
 export const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900"> 
-      {/* <div className='fixed top-0 -z-10 h-full w-full'> */}
+      <div className='fixed top-0 -z-10 h-full w-full'>
       {/* <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>    */}
-      {/* <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div> */}
-      {/* </div> */}
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      </div>
       <div className='container mx-auto px-8'>
       <Navbar />
       <Hero />
